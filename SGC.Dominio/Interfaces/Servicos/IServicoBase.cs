eb92de8cf8c5421a24e5a601f565e31316c8c61a
Eduardo Oliveira <eduardooliveira_jr@hotmail.com>
2018-07-05 13:46:24 -0300

    Criação da Solution e da Classe de Dominio

---
+++ SGC.Dominio/Interfaces/Servicos/IServicoBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace SGC.Dominio.Interfaces.Servicos
+{
+    public interface IServicoBase<T> : IDisposable where T : class
+    {
+        IList<T> Buscar(Expression<Func<T, bool>> where);
+        IList<T> Buscar(string _include, Expression<Func<T, bool>> where);
+        IList<T> ListarTodos();
+        void Inserir(T obj);
+        void Alterar(T obj);
+        void Excluir(T obj);    
+    }
+}
